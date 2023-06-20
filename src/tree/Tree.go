5c5dee67514ce1dd3712fda36c4bf17810fdc5ca
allantabilog <allan.tabilog@gmail.com>
2023-06-20 19:47:38 +1000

    feat: depth-first tree traversals

--- src/tree/Tree.go
+++ src/tree/Tree.go
@@ -36,6 +36,49 @@ func (node *Node) displayIndented(indent string, depth int) string {
 	if node.right != nil {
 		result = result + node.right.displayIndented(indent, depth+1)
 	}
+	return result
+}
+
+func (node *Node) preorder() string {
+	var result = node.data
+
+	if node.left != nil {
+		result += " " + node.left.preorder()
+	}
+	if node.right != nil {
+		result += " " + node.right.preorder()
+	}
+
+	return result
+}
+
+func (node *Node) inorder() string {
+	var result string
+
+	if node.left != nil {
+		result += node.left.inorder() + " "
+	}
+
+	result += node.data
+
+	if node.right != nil {
+		result += " " + node.right.inorder()
+	}
+
+	return result
+}
+
+func (node *Node) postorder() string {
+	var result string
+
+	if node.left != nil {
+		result += node.left.postorder() + " "
+	}
+
+	if node.right != nil {
+		result += node.right.postorder() + " "
+	}
+	result += node.data
 
 	return result
 }
--- src/tree/Tree_test.go
+++ src/tree/Tree_test.go
@@ -1,14 +1,41 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestBuildTree(t *testing.T) {
-	assert.True(t, true)
 	var tree = buildTree()
-	fmt.Println(tree.root.displayIndented(" ", 0))
+	var expectedOutput = `A
+ B
+  D
+  E
+   G
+ C
+  F
+   H
+    I
+    J
+`
+	assert.Equal(t, expectedOutput, tree.root.displayIndented(" ", 0))
+}
+
+func TestPreorder(t *testing.T) {
+	var tree = buildTree()
+	var expectedTraversal = "A B D E G C F H I J"
+	assert.Equal(t, expectedTraversal, tree.root.preorder())
+}
+
+func TestInorder(t *testing.T) {
+	var tree = buildTree()
+	var expectedTraversal = "D B G E A C I H J F"
+	assert.Equal(t, expectedTraversal, tree.root.inorder())
+}
+
+func TestPostorder(t *testing.T) {
+	var tree = buildTree()
+	var expectedTraversal = "D G E B I J H F C A"
+	assert.Equal(t, expectedTraversal, tree.root.postorder())
 }

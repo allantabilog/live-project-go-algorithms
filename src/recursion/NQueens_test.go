6ebea988aeff5aaea03e6cc18e58f7c3c5aa94c1
allantabilog <allan.tabilog@gmail.com>
2023-07-09 09:34:46 +1000

    isSeriesLegal() tests

--- src/recursion/NQueens_test.go
+++ src/recursion/NQueens_test.go
@@ -10,12 +10,11 @@ func TestDrive(t *testing.T) {
 	NQueensDriver()
 }
 
-func TestSeriesIsLegal(t *testing.T) {
-
+func TestSeriesIsLegal_Rows(t *testing.T) {
 	// generate some test boards
 	var boardGood = MakeBoard[string](10, 10, empty)
 	boardGood[0][0] = queen
-	boardGood[2][3] = queen
+	boardGood[1][0] = queen
 	Trace(boardGood)
 
 	var boardBad = MakeBoard[string](10, 10, empty)
@@ -23,32 +22,47 @@ func TestSeriesIsLegal(t *testing.T) {
 	boardBad[0][3] = queen
 	Trace(boardBad)
 
-	var boardBad2 = MakeBoard[string](10, 10, empty)
-	boardBad2[0][0] = queen
-	boardBad2[1][1] = queen
-	Trace(boardBad2)
-
-
 	// boardGood passes row series test (no adjacent queens in same row)
-	assert.Equal(t, true, seriesIsLegal(boardGood, 10, 0, 0, 1, 0))
+	assert.Equal(t, true, seriesIsLegal(boardGood, 10, 0, 0, 0, 1), "Board has < 2 queens in a row")
 
-	// boardGood passes column series test (no adjacent queens in same col)
-	assert.Equal(t, true, seriesIsLegal(boardGood, 10, 0, 0, 0, 1))
-	
-	// boardBad passes row series
-	assert.Equal(t, true, seriesIsLegal(boardBad, 10, 0, 0, 1, 0))
+	// boardBad fails row series 
+	assert.Equal(t, false, seriesIsLegal(boardBad, 10, 0, 0, 0, 1), "Board has >= 2 queens in a row")
+}
+
+func TestSeriesIsLegal_Columns(t *testing.T) {
+	// generate some test boards
+	var boardGood = MakeBoard[string](10, 10, empty)
+	boardGood[0][0] = queen
+	boardGood[0][1] = queen
+	Trace(boardGood)
+
+	var boardBad = MakeBoard[string](10, 10, empty)
+	boardBad[0][0] = queen
+	boardBad[1][0] = queen
+	Trace(boardBad)
+
+	// boardGood passes column series
+	assert.Equal(t, true, seriesIsLegal(boardGood, 10, 0, 0, 1, 0), "Board has < 2 queens in a column")
 
 	// boardBad fails column series 
-	assert.Equal(t, false, seriesIsLegal(boardBad, 10, 0, 0, 0, 1))
+	assert.Equal(t, false, seriesIsLegal(boardBad, 10, 0, 0, 1, 0), "Board has >= 2 queens in a column")
+}
 
-	// boardBad2 passes row series
-	assert.Equal(t, true, seriesIsLegal(boardBad2, 10, 0, 0, 1, 0))
-	
-	// boardBad2 passes column series
-	assert.Equal(t, true, seriesIsLegal(boardBad2, 10, 0, 0, 0, 1))
+func TestSeriesIsLegal_Diagonal(t *testing.T) {
+	// generate some test boards
+	var boardGood = MakeBoard[string](10, 10, empty)
+	boardGood[0][0] = queen
+	boardGood[0][1] = queen
+	Trace(boardGood)
 
-	// boardBad2 fails diagonal series
-	assert.Equal(t, false, seriesIsLegal(boardBad2, 10, 0, 0, 1, 1))
-	
+	var boardBad = MakeBoard[string](10, 10, empty)
+	boardBad[0][0] = queen
+	boardBad[1][1] = queen
+	Trace(boardBad)
 
+	// boardGood passes column series
+	assert.Equal(t, true, seriesIsLegal(boardGood, 10, 0, 0, 1, 1), "Board has < 2 queens in a diagonal")
+
+	// boardBad fails column series 
+	assert.Equal(t, false, seriesIsLegal(boardBad, 10, 0, 0, 1, 1), "Board has >= 2 queens in a diagonal")
 }
